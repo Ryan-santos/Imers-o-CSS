8036045a0956cb252f5a85f6ab6e5a8a68fba147
Ryan-santos <65314533+Ryan-santos@users.noreply.github.com>
2021-12-17 00:45:04 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,3 +6,31 @@ function toggleMenu() {
 }
 
 bntMobile.addEventListener('click', toggleMenu);
+
+window.onscroll = function(){
+    // O que faz mudar a cor do menu deacordo com a posicao do scroll
+    var top = window.pageYOffset || document.documentElement.scrollTop
+    const link_nav = document.querySelectorAll('.link_nav');
+    console.log(top)
+    if( top >= 0 ) {
+        link_nav.forEach((item) => 
+        item.classList.remove('active'));
+    }if( top >= 700 ) {
+        link_nav.forEach((item) => 
+        item.classList.remove('active'));
+        link_nav[0].classList.add('active');
+    }if( top >= 1200 ) {
+        link_nav.forEach((item) => 
+        item.classList.remove('active'));
+        link_nav[1].classList.add('active');
+    }if( top >= 1700 ) {
+        link_nav.forEach((item) => 
+        item.classList.remove('active'));
+        link_nav[2].classList.add('active');
+    }if( top >= 2050 ) {
+        link_nav.forEach((item) => 
+        item.classList.remove('active'));
+        link_nav[3].classList.add('active');
+    }
+
+}
